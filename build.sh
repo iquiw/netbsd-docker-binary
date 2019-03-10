62f7025c8bdcebd7baab997fa87562e56a107a18
Iku Iwasa <iku.iwasa@gmail.com>
2019-03-10 15:12:13 +0900

    Archive Docker CLI binaries with OS name

--- build.sh
+++ build.sh
@@ -9,7 +9,9 @@ export BUILDTIME=$(date --utc --rfc-3339 ns 2> /dev/null | sed -e 's/ /T/')
 export GOOS=netbsd
 
 build() {
-	GOARCH="$1" go build -v -o docker-"$1"
+	arch=$1
+	GOARCH="$arch" go build -v -o docker-"$GOOS"-"$arch"
+	xz docker-"$arch"
 }
 
 cd /go/src/github.com/docker/cli/cmd/docker
