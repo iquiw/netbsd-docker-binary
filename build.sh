d37825d3bb6909b7aa6bfa15a5dce6de3fce6ba3
Iku Iwasa <iku.iwasa@gmail.com>
2019-03-11 23:40:42 +0900

    Embed version, build info into binary

--- build.sh
+++ build.sh
@@ -2,18 +2,24 @@
 
 set -ex
 
-export VERSION=18.09.3
-export GITCOMMIT=$(git rev-parse --short HEAD)
-export BUILDTIME=$(date --utc --rfc-3339 ns 2> /dev/null | sed -e 's/ /T/')
+VERSION=18.09.3
+GITCOMMIT=$(git rev-parse --short HEAD)
+BUILDTIME=$(date --utc --rfc-3339 ns 2> /dev/null | sed -e 's/ /T/')
+
+LDFLAGS="-X github.com/docker/cli/cli.Version=$VERSION \
+-X github.com/docker/cli/cli.GitCommit=$GITCOMMIT \
+-X github.com/docker/cli/cli.BuildTime=$BUILDTIME"
 
 export GOOS=netbsd
 
 ARTIFACTS_DIR=/tmp/artifacts
 
 build() {
-	arch=$1
-	GOARCH="$arch" go build -v -o "$ARTIFACTS_DIR/docker-$GOOS-$arch"
-	xz "$ARTIFACTS_DIR/docker-$GOOS-$arch"
+	export GOARCH=$1
+	go build -v \
+	   -ldflags "$LDFLAGS" \
+	   -o "$ARTIFACTS_DIR/docker-$GOOS-$GOARCH"
+	xz "$ARTIFACTS_DIR/docker-$GOOS-$GOARCH"
 }
 
 cd /go/src/github.com/docker/cli/cmd/docker
